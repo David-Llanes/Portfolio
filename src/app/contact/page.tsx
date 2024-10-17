f924a943a5e2ea14f64204f43e088f54329d93ea
David Alejandro Llanes Bojorquez <david.llanes1019@gmail.com>
2024-10-16 23:01:17 -0700

    fix: the title of the contact page is now related to the page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'About me',
+  title: 'Contact',
   description:
     'Get in touch with David Llanes, a software engineer specializing in full-stack web development, open to internships, part-time jobs, and eager to learn and grow in new technologies.',
 }
